0b25949c4ff02cdea111ee0bafa5c63a768fb63a
Datonte Dakio-Horsfall <tontehorsefal@gmail.com>
2026-02-05 20:31:44 +0100

    Change website name from McDonald's to work throughout the site

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     // We could handle the click here, but using direct href="prank.html" is simpler.
     // Keeping this for potential future enhancements (e.g. analytics or delay).
-    console.log("McDonald's Careers Mirror Loaded");
+    console.log("work Loaded");
     
     // Rotate favicon 180 degrees
     const rotateFavicon = () => {
